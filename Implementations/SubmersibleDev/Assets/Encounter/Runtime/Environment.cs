b91d9e52f069600bf476c20f607f3b68fc3c7c20
Charlie Huguenard <charliehuge@users.noreply.github.com>
2025-07-16 17:05:53 -0400

    stub XR previs

---
+++ Implementations/SubmersibleDev/Assets/Encounter/Runtime/Environment.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Encounter.Runtime
+{
+    public class Environment : MonoBehaviour
+    {
+        [SerializeField] private Transform playerSpawnPoint;
+        
+        public Transform PlayerSpawnPoint => playerSpawnPoint;
+    }
+}

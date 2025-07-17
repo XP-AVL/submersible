be4f972c9259d21491cc589bbdb6ed6aae26f8a6
Charlie Huguenard <charliehuge@users.noreply.github.com>
2025-07-17 15:51:19 -0400

    load scene for environment, make spawn point its own thing

--- Implementations/EncounterPrototype/Assets/Encounter/Runtime/Startup.cs
+++ Implementations/EncounterPrototype/Assets/Encounter/Runtime/Startup.cs
@@ -1,28 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Encounter.Runtime
 {
+    /// <summary>
+    /// Starts up the experience and goes away.
+    /// </summary>
     public class Startup : MonoBehaviour
     {
-        [SerializeField] private Environment environmentPrefab;
+        [SerializeField] private string environmentSceneName;
         [SerializeField] private Player playerPrefab;
-
-        private Environment _environment;
-        private Player _player;
         
         private void OnEnable()
         {
-            _environment = Instantiate(environmentPrefab);
-            _player = Instantiate(
-                playerPrefab, 
-                _environment.PlayerSpawnPoint.position, 
-                _environment.PlayerSpawnPoint.rotation);
+            DontDestroyOnLoad(gameObject);
+            
+            // Load the environment
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.LoadScene(environmentSceneName, LoadSceneMode.Additive);
         }
 
-        private void OnDisable()
+        private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
         {
-            Destroy(_environment);
-            Destroy(_player);
+            if (scene.name != environmentSceneName)
+            {
+                Debug.LogWarning($"SceneManager.OnSceneLoaded: Unexpected scene loaded: {scene.name}");
+                return;
+            }
+            
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            
+            // Find the Player Spawn, if there is one
+            var playerSpawn = FindFirstObjectByType<PlayerSpawn>();
+
+            if (playerSpawn == null)
+            {
+                Debug.LogWarning("Environment not found in scene. Using default environment values.");
+            }
+            
+            // Spawn the player
+            var playerSpawnPos = playerSpawn ? playerSpawn.transform.position : Vector3.zero;
+            var playerSpawnRot = playerSpawn ? playerSpawn.transform.rotation : Quaternion.identity;
+            Instantiate(playerPrefab, playerSpawnPos, playerSpawnRot);
+            
+            // Clean up
+            Destroy(gameObject);
         }
     }
 }
